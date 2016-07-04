31061b8a16e433a45bd68d78a53374c79e0cb27d
Jim Gilmartin <jim@feisinfo.com>
2016-07-04 04:54:32 -0700

    First initial files to get the application running.

---
+++ UWPApis/UWPApiUI/Constants/PageTokens.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPApiUI.Constants
+{
+    static class PageTokens
+    {
+        public const string MainPage = "Main";
+    }
+}
